207ae65a320a8c3aff8cbbd5e4839b5b80a1f296
NationsAnarchy <en.deathraven@gmail.com>
2021-03-24 21:32:44 +0700

    Commit Day 4 Java Core

--- Java Assignment/Testing_Assignment_4/src/com/vti/academy/backend/Exercise1Constructor.java
+++ Java Assignment/Testing_Assignment_4/src/com/vti/academy/backend/Exercise1Constructor.java
@@ -7,5 +7,4 @@ import com.vti.academy.entity.Student;
 public class Exercise1Constructor {
     Department department = new Department();
     Account account = new Account();
-    Student student = new Student();
 }
