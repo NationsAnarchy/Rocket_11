c5476bd813f7c6e64ec32181b2079e19caa158c8
NationsAnarchy <en.deathraven@gmail.com>
2021-03-24 20:38:49 +0700

    Commit day 4 Java

---
+++ Java Assignment/Testing_Assignment_4/src/Account.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+
+public class Account {
+    int id;
+    String email;
+    String username;
+    String fullName;
+    Department department;
+    Position position;
+    LocalDate createDate;
+    Group[] groups;
+
+    Account() {
+    }
+
+    Account(int id, String email, String username, String firstName, String lastName) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.fullName = firstName + " " + lastName;
+    }
+
+    Account(String email, String username, String firstName, String lastName, Position position) {
+        this.email = email;
+        this.username = username;
+        this.fullName = firstName + " " + lastName;
+        this.position = position;
+        this.createDate = LocalDate.now();
+    }
+
+    Account(int id, String email, String username, String firstName, String lastName, Position position,
+            LocalDate createDate) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.fullName = firstName + " " + lastName;
+        this.position = position;
+        this.createDate = createDate;
+    }
+}
---
+++ Java Assignment/Testing_Assignment_4/src/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+        Student student1 = new Student(1, "Charles");
+
+        Department department1 = new Department("Sales");
+        Department department2 = new Department();
+
+        Account account1 = new Account();
+    }
+}
