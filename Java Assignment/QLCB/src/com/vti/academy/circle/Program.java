a0f3ac5e94f0ec2310c77b9a251080e6ed5e15f2
NationsAnarchy <en.deathraven@gmail.com>
2021-03-29 17:22:39 +0700

    Update bài tập Day 4 Java Core

---
+++ Java Assignment/QLCB/src/com/vti/academy/circle/Program.java
@@ -0,0 +1,23 @@
+package com.vti.academy.circle;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		Circle circle1 = new Circle();
+		
+		System.out.print("Nhập vào bán kính của hình tròn: ");
+		
+		double radius = scanner.nextDouble();
+		scanner.nextLine();
+		circle1.setRadius(radius);
+		scanner.close();
+		
+		System.out.println("Tính diện tích hình tròn với bán kính đã nhập ở trên");
+		System.out.println("----------------------------------------------------");
+		System.out.println("Bán kính: " + circle1.getRadius());
+		System.out.println("Diện tích: " + Math.ceil(circle1.getArea() * 100) / 100);
+	}
+}
