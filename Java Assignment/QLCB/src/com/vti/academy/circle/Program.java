963bbc003b1f539cceb1f6311199a3e14cf9d19d
NationsAnarchy <en.deathraven@gmail.com>
2021-03-29 17:46:26 +0700

    Bổ sung bài hình tròn Day 4 Java Core

--- Java Assignment/QLCB/src/com/vti/academy/circle/Program.java
+++ Java Assignment/QLCB/src/com/vti/academy/circle/Program.java
@@ -9,15 +9,19 @@ public class Program {
 		Circle circle1 = new Circle();
 		
 		System.out.print("Nhập vào bán kính của hình tròn: ");
-		
 		double radius = scanner.nextDouble();
 		scanner.nextLine();
 		circle1.setRadius(radius);
+		
+		System.out.print("Nhập vào màu sắc của hình tròn: ");
+		String color = scanner.nextLine();
+		circle1.setColor(color);
 		scanner.close();
 		
 		System.out.println("Tính diện tích hình tròn với bán kính đã nhập ở trên");
 		System.out.println("----------------------------------------------------");
 		System.out.println("Bán kính: " + circle1.getRadius());
+		System.out.println("Màu sắc: " + circle1.getColor());
 		System.out.println("Diện tích: " + Math.ceil(circle1.getArea() * 100) / 100);
 	}
 }
