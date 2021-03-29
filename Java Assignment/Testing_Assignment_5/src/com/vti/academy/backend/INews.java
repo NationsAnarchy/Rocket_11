f68452763989bb98d221fd4309267c1436bd76c5
NationsAnarchy <en.deathraven@gmail.com>
2021-03-29 20:23:02 +0700

    Update Java Core Day 5

---
+++ Java Assignment/Testing_Assignment_5/src/com/vti/academy/backend/INews.java
@@ -0,0 +1,7 @@
+package com.vti.academy.backend;
+
+public interface INews {
+    void display();
+
+    float calculate(int[] rate);
+}
---
+++ Java Assignment/Testing_Assignment_5/src/com/vti/academy/entity/News.java
@@ -0,0 +1,76 @@
+package com.vti.academy.entity;
+
+import com.vti.academy.backend.INews;
+
+import java.util.Arrays;
+
+public class News implements INews {
+    private int id;
+    private String title;
+    private String publishDate;
+    private String author;
+    private String content;
+    private float averageRate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public float getAverageRate() {
+        return averageRate;
+    }
+
+    public void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Publish date: " + publishDate);
+        System.out.println("Author: " + author);
+        System.out.println("Content: " + content);
+        System.out.println("Average rate: " + averageRate);
+    }
+
+    @Override
+    public float calculate(int[] rate) {
+        int total = 0;
+        for (int i = 0; i < rate.length; i++){
+            total += rate[i];
+        }
+        averageRate = total / rate.length;
+        return averageRate;
+    }
+}
