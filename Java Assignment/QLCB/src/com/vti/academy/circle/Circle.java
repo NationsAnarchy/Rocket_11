5422d76a56fe5d3ee2b4561aa4092415de35ac13
NationsAnarchy <en.deathraven@gmail.com>
2021-03-31 15:48:54 +0700

    Update bài tập Day 4 Java Core

--- Java Assignment/QLCB/src/com/vti/academy/circle/Circle.java
+++ Java Assignment/QLCB/src/com/vti/academy/circle/Circle.java
@@ -1,37 +1,38 @@
 package com.vti.academy.circle;
+
 public class Circle {
-	private double radius;
-	private String color;
+    private double radius;
+    private String color;
 
-	public Circle() {
-	}
+    public Circle() {
+    }
 
-	public Circle(double radius) {
-		this.radius = radius;
-	}
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
-	public Circle(double radius, String color) {
-		this.radius = radius;
-		this.color = color;
-	}
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
 
-	public double getRadius() {
-		return radius;
-	}
+    public double getRadius() {
+        return radius;
+    }
 
-	public void setRadius(double radius) {
-		this.radius = radius;
-	}
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 
-	public String getColor() {
-		return color;
-	}
+    public String getColor() {
+        return color;
+    }
 
-	public void setColor(String color) {
-		this.color = color;
-	}
+    public void setColor(String color) {
+        this.color = color;
+    }
 
-	public double getArea() {
-		return Math.PI * Math.pow(radius, 2.0);
-	}	
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2.0);
+    }
 }
