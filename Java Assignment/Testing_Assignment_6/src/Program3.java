d28df952c5a403a9c36baf9183bcf50de510bb23
NationsAnarchy <en.deathraven@gmail.com>
2021-04-03 01:48:46 +0700

    Edit Day 6 Java Core

--- Java Assignment/Testing_Assignment_6/src/Program3.java
+++ Java Assignment/Testing_Assignment_6/src/Program3.java
@@ -6,8 +6,7 @@ public class Program3 {
             Scanner scanner = new Scanner(System.in);
             int[] numbers = new int[]{1, 2, 3};
             System.out.println("Which position do you want to get from the list?");
-            int position = scanner.nextInt();
-            scanner.close();
+            int position = scanner.nextInt();            
             System.out.println("Here's the value of the input position.");
             System.out.println(numbers[position]);
         }
--- Java Assignment/Testing_Assignment_6/src/Program5and6.java
+++ Java Assignment/Testing_Assignment_6/src/Program5and6.java
@@ -4,26 +4,23 @@ public class Program5and6 {
     public static void main(String[] args) {
         // Question 5 + 6
         System.out.print("Please input your age here: ");
-        inputAge();
+        inputAge();        
     }
 
     public static int inputInt(String message) {
         Scanner scanner = new Scanner(System.in);
-        scanner.close();     
-
         while (true) {
             try {
                 return Integer.parseInt(scanner.nextLine().trim());
             } catch (Exception e) {
                 System.err.println(message);
-            }
-        }
+            }            
+        }        
     }
 
     public static void inputAge() {
         while (true) {
             int age = inputInt("Incorrect input, please use an int. Please input again.");
-
             if (age <= 0) {
                 System.err.println("Incorrect age, must be a number greater than zero. Please input again.");
             } else {
