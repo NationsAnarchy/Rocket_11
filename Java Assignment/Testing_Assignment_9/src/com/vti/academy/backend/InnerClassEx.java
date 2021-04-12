ff55565cab76051ec7c938b4ed9eadea8ffceb16
NationsAnarchy <en.deathraven@gmail.com>
2021-04-12 17:16:03 +0700

    Update Day 9 Java Core

--- Java Assignment/Testing_Assignment_9/src/com/vti/academy/backend/InnerClassEx.java
+++ Java Assignment/Testing_Assignment_9/src/com/vti/academy/backend/InnerClassEx.java
@@ -1,5 +1,41 @@
 package com.vti.academy.backend;
 
 public class InnerClassEx {
+    public class Car {
+        private String carName;
+        private String carType;
+        private String carEngine;
 
+        public Car(String carName, String carType) {
+            this.carName = carName;
+            this.carType = carType;
+        }
+
+        public void setCarEngine(String engine) {
+            this.carEngine = engine;
+        }
+
+        public void showInformation() {
+            System.out.println("Car name: " + carName);
+            System.out.println("Car type: " + carType);
+            System.out.println("Car engine: " + carEngine);
+        }
+
+        public class Engine {
+            private String engineType;
+
+            public String getEngineType() {
+                return engineType;
+            }
+
+            public void setEngineType(String engineType) {
+                this.engineType = engineType;
+            }
+
+            @Override
+            public String toString() {
+                return engineType;
+            }
+        }
+    }
 }
--- Java Assignment/Testing_Assignment_9/src/com/vti/academy/frontend/Program.java
+++ Java Assignment/Testing_Assignment_9/src/com/vti/academy/frontend/Program.java
@@ -6,6 +6,7 @@ public class Program {
     public static void main(String[] args) {
         // Question 1:
         @SuppressWarnings("depreciation")
-        Date date = new Date(2020, 05, 18);
+        Date date = new Date(2020, 5, 18);
+        System.out.println(date);
     }
 }
