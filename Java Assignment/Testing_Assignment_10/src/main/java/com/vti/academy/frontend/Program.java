529bdb761d65d3a7c2cbf968d8599b849ff6e648
NationsAnarchy <en.deathraven@gmail.com>
2021-04-14 17:54:19 +0700

    Update Day 10 Java Core

---
+++ Java Assignment/Testing_Assignment_10/src/main/java/com/vti/academy/frontend/Program.java
@@ -0,0 +1,13 @@
+package com.vti.academy.frontend;
+
+/**
+ * Hello world!
+ *
+ */
+public class Program
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
